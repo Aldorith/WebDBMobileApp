a3191ee9fd58164ce24d04106a0c9532acd2f5dc
Aldorith <connorjohnbacon@gmail.com>
2022-04-26 17:42:38 -0700

    -Added Submit Button

--- app/src/main/java/com/example/webdbmobileapp/MainActivity.java
+++ app/src/main/java/com/example/webdbmobileapp/MainActivity.java
@@ -1,5 +1,27 @@
 package com.example.webdbmobileapp;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity  {
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        Button submit_button = findViewById(R.id.submit_button);
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.i("Submit Button Pressed", "The Submit Button successfully was called");
+            }
+        });
+    }
 }
