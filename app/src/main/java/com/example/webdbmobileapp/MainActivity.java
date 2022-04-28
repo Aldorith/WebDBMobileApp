beae90998eb09d7fd91113dd977366d01831ea84
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 20:27:32 -0700

    Kinda Ugly, But its Done

--- app/src/main/java/com/example/webdbmobileapp/MainActivity.java
+++ app/src/main/java/com/example/webdbmobileapp/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.webdbmobileapp;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
 
         EditText editTextToDoTitle = findViewById(R.id.ToDoTitle);
         EditText editTextToDoDescription = findViewById(R.id.ToDoDescription);
+        ListView listView = findViewById(R.id.ToDoDisplay);
 
         Button submit_button = findViewById(R.id.submit_button);
         submit_button.setOnClickListener(v -> {
@@ -40,5 +51,28 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("Task Submission Failure", "Task Failed to Submit");
             }
         });
+
+        ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this ,R.layout.list_view, list);
+        listView.setAdapter(adapter);
+
+        DatabaseReference retrieve = FirebaseDatabase.getInstance("https://web-db-mobile-app-7e48d-default-rtdb.firebaseio.com/").getReference().child("tasks");
+        retrieve.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
+
 }
